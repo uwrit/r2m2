6781b9dac1c6a31aac0147e014f71cc1b7a3b5f2
Mehadi Hassan <47397042+mh2727@users.noreply.github.com>
2020-03-12 22:38:13 -0700

    removed hardcoded login

--- src/ui-client/src/reducers/login.ts
+++ src/ui-client/src/reducers/login.ts
@@ -10,8 +10,8 @@ import { LoginState, LoginServerCommunicationState } from "../model/LoginState"
 
 export const defaultLoginState = (): LoginState => {
     return {
-        emailAddress: 'ndobb@uw.edu',
-        entryCode: 'ntest',
+        emailAddress: '',
+        entryCode: '',
         loggedIn: false,
         serverCommunication: LoginServerCommunicationState.Idle
     }
