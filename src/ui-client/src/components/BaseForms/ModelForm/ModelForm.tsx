e719f7198ab318a34a0b4bb705f3965ab431a34e
Mehadi Hassan <mehadi@Mehadis-MacBook-Pro.local>
2020-03-13 09:55:28 -0700

    updating ModelForm

--- src/ui-client/src/components/BaseForms/ModelForm/ModelForm.tsx
+++ src/ui-client/src/components/BaseForms/ModelForm/ModelForm.tsx
@@ -64,7 +64,7 @@ export class ModelForm extends React.PureComponent<Props,State> {
                                 <FiChevronLeft />   
                                 Go Back
                             </button>
-                            <button className={`maturity-model-button next`} onClick={this.handleNextClick}>
+                            <button className={`maturity-model-button secondarynext`} onClick={this.handleNextClick}>
                                 Next
                                 <FiChevronRight />
                             </button>
@@ -182,12 +182,10 @@ export class ModelForm extends React.PureComponent<Props,State> {
             }) as UserAnswers;
             dispatch(userSetAnswers(cpy));
             this.handleSingleAnswerClick(isFirst, isLast, total);
+            return;
         }
 
-        if (question.type === QuestionType.MultipleAnswer) {
-            // const cpy = Object.assign
-            this.handleMultipleAnswerClick(value, isLast, alreadyCompleted);
-        }
+        this.handleMultipleAnswerClick(value, isLast, alreadyCompleted);
     }
 
     private handleMultipleAnswerClick = (value: any, isLast: boolean, alreadyCompleted: boolean) => {
